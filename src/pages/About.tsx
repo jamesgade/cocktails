8e1a40d22a9396154438442ef5bb003043e38058
jamesgade <gadejames48@gmail.com>
2022-11-09 09:30:38 +0530

    cocktails db

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt ducimus earum similique quis eum voluptatibus quibusdam consequatur, maiores quasi omnis sequi animi repellendus sit inventore culpa ratione quas sed incidunt tenetur sunt aliquam libero. Vel fuga ipsum ad provident recusandae.</p>
+    </section>
+  )
+}
+
+export default About
